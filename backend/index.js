4a6ba6552bb824b01a7fe30c42105f764e6a28e6
Bekir Faruk ARABACI <113997401+BekirFarukARABACI@users.noreply.github.com>
2024-04-01 05:37:41 +0300

    feat:  Collections Added

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose")
 const express = require("express")
 const app = express()
-const {v4:uuidv4}= require("uuid")
+const { v4: uuidv4 } = require("uuid")
 const multer = require("multer")
 const cors = require("cors")
 
@@ -10,16 +10,62 @@ app.use(express.json())
 
 
 const uri = "mongodb+srv://MongoDb:1@reactecommercedb.jmsuysu.mongodb.net/?retryWrites=true&w=majority"
-mongoose.connect(uri).then(()=>{
+mongoose.connect(uri).then(() => {
     console.log("Database bağlantısı başarılı")
-}).catch((err)=>{
+}).catch((err) => {
     console.log(err.message)
 })
+
+//User Collection
+const userSchema = new mongoose.Schema({
+    _id: String,
+    name: String,
+    email: String,
+    password: String
+})
+
+const User = mongoose.model("User", userSchema)
+//User Collection
+
+//Product Collection
+const productSchema = new mongoose.Schema({
+    _id: String,
+    name: String,
+    stock: Number,
+    price: Number,
+    imageUrl: String
+})
+const Product = mongoose.model("Product", productSchema)
+//Product Collection
+
+//Basket Collection
+const basketScheme = new mongoose.Schema({
+    _id: String,
+    productID: String,
+    userID: String,
+    count: Number,
+    price: Number
+})
+
+const Basket = mongoose.model("Basket", basketScheme)
+//Basket Collection
+
+//Order Collection
+const orderScheme = new mongoose({
+    _id: String,
+    productID: String,
+    userID: String,
+    count: Number,
+    price: Number
+})
+
+const Order = mongoose.model("Order", orderScheme)
+//Order Collection
 
 
 
 const port = 5000;
-app.listen(5000,()=>{
-   console.log("Uygulama http://localhost:" + port + " üzerinden ayakta") 
+app.listen(5000, () => {
+    console.log("Uygulama http://localhost:" + port + " üzerinden ayakta")
 })
 
