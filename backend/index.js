e2baf9866f8cc36f979e30ab823d13fd78467ffe
Bekir Faruk ARABACI <113997401+BekirFarukARABACI@users.noreply.github.com>
2024-04-01 05:49:48 +0300

    feat : register and login method added

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,7 @@ const app = express()
 const { v4: uuidv4 } = require("uuid")
 const multer = require("multer")
 const cors = require("cors")
+const jwt = require("jsonwebtoken")
 
 app.use(cors())
 app.use(express.json())
@@ -62,7 +63,58 @@ const orderScheme = new mongoose({
 const Order = mongoose.model("Order", orderScheme)
 //Order Collection
 
+//Token
+const secretKey = "Gizli anahtarım Gizli anahtarım Gizli anahtarım"
+const options = {
+    expiresIn : "1h"
+}
+//Token
 
+//Register Method
+app.post("/auth/register",async(req,res)=>{
+    try{
+        const {name,email,password} = req.body;
+        let user = new User({
+            _id: uuidv4(),
+            name: name,
+            email: email,
+            password: password
+        })
+
+        await user.save()
+        const payLoad = {
+            user: user
+        }
+        const token = jwt.sign(payLoad,secretKey,options)
+        res.json({user:user, token:token})
+
+    }catch(error){
+        res.status(500).json({error:error.message})
+    }
+})
+//Register Method
+
+
+//Login Method
+app.post("/auth/login",async(req,res)=>{
+    try {
+        const {email,password}=req.body
+        const users = await User.find({email:email,password:password})
+        if(users.length === 0){
+            res.status(500).json({message: "Kullanıcı mail adresi ya da şifre yanlış"})
+        }
+        else{
+            const payLoad = {
+                user: users[0]
+            }
+            const token = jwt.sign(payLoad,secretKey,options)
+            res.json({user:users[0], token:token})
+        }
+    } catch (error) {
+        
+    }
+});
+//Login Method
 
 const port = 5000;
 app.listen(5000, () => {
